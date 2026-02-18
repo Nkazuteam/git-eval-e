1432c669d333d8b42284ce9b6ce008296db5af0b
unknown <tomoki.py@gmail.com>
2026-02-18 20:19:06 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ def fizzbuzz(n):
     results = []
     for i in range(1, n + 1):
         if i % 15 == 0:
-            results.append("FizzBuzz")
+            results.append("Fizzbuzz")
         elif i % 3 == 0:
             results.append("Fizz")
         elif i % 5 == 0:
