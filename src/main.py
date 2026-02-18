0557a269479d1a6f396673ddd90d74242c295a02
unknown <tomoki.py@gmail.com>
2026-02-18 20:05:54 +0900

    add main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+def fizzbuzz(n):
+    results = []
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            results.append("FizzBuzz")
+        elif i % 3 == 0:
+            results.append("Fizz")
+        elif i % 5 == 0:
+            results.append("Buzz")
+        else:
+            results.append(str(i))
+    return results
+
+
+def main():
+    results = fizzbuzz(30)
+    for item in results:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
